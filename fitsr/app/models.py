f9d0d6810cb33a5363b58ddd71d8d2e1a7643af1
Víctor R. Ruiz <vrruiz@gmail.com>
2013-02-05 03:18:00 +0000

    Import FITS header into ImageMetadata upon image creation

--- fitsr/app/models.py
+++ fitsr/app/models.py
@@ -1,4 +1,10 @@
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.conf import settings
+
+from os.path import join
+from pyfits import getdata
 
 # Create your models here.
 class Ccd(models.Model):
@@ -98,17 +104,60 @@ class Image(models.Model):
 
 
 class ImageMetadata(models.Model):
+    BOOLEAN = 'BOOLEAN'
+    INTEGER = 'INTEGER'
+    TEXT = 'TEXT'
+    DATE = 'DATE'
+    DECIMAL = 'DECIMAL'
+    FLOAT = 'FLOAT'
+    CHAR = 'CHAR'
+    VALUE_TYPES = (
+        (BOOLEAN, 'Boolean'),
+        (INTEGER, 'Integer'),
+        (TEXT, 'Text'),
+        (DATE, 'Date'),
+        (DECIMAL, 'Decimal'),
+        (FLOAT, 'Float'),
+        (CHAR, 'Char'),
+    )
     image = models.ForeignKey(Image)
     key = models.CharField(max_length=200, db_index=True)
-    value_type = models.CharField(max_length=200)
+    value_type = models.CharField(max_length=200, choices=VALUE_TYPES, default=TEXT)
     boolean_value = models.NullBooleanField(null=True)
     integer_value = models.IntegerField(null=True)
     text_value = models.TextField(null=True)
     date_value = models.DateTimeField(null=True)
     decimal_value = models.DecimalField(max_digits=10, decimal_places=2, null=True)
+    float_value = models.FloatField(null=True)
     char_value = models.CharField(max_length=200, null=True)
     creation_date = models.DateTimeField(auto_now=True)
     
     def __unicode__(self):
-        return self.key + ' ' + self.image.image_file.url
+        return self.key
+
+@receiver(post_save, sender=Image)
+def save_image_metadata(sender, **kwargs):
+    """ Stores image metadata in table """
+    image = kwargs.get('instance', False)
+    if (image):
+        image_path = join(settings.MEDIA_ROOT, image.image_file.name)
+        image_data, header = getdata(image_path, header=True)
+        for key in header.keys():
+            metadata = ImageMetadata()
+            metadata.image = image
+            metadata.key = key
+            value = header[key]
+            if (type(value) == int):
+                metadata.integer_value = value
+                metadata.value_type = metadata.INTEGER
+            elif (type(value) == bool):
+                metadata.boolean_value = value
+                metadata.value_type = metadata.BOOLEAN
+            elif (type(value) == str):
+                metadata.text_value = value
+                metadata.value_type = metadata.TEXT
+            elif (type(value) == float):
+                metadata.float_value = value
+                metadata.value_type = metadata.INTEGER
+            metadata.save()
 

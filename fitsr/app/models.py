c6fb5bb2e0871c183dc122e9fad4d1ac670b0de7
Víctor R. Ruiz <vrruiz@gmail.com>
2013-02-06 01:07:19 +0000

    Save exposure time and image date in Image table

--- fitsr/app/models.py
+++ fitsr/app/models.py
@@ -4,6 +4,7 @@ from django.dispatch import receiver
 from django.conf import settings
 
 from os.path import join
+from dateutil import parser
 from pyfits import getdata
 
 # Create your models here.
@@ -32,9 +33,9 @@ class Telescope(models.Model):
 class Site(models.Model):
     name = models.CharField(max_length=200)
     code = models.CharField(max_length=3)
-    altitude = models.IntegerField(null=True)
-    latitude = models.DecimalField(max_digits=9, decimal_places=6, null=True)
-    longitude = models.DecimalField(max_digits=9, decimal_places=6, null=True)
+    altitude = models.IntegerField(blank=True)
+    latitude = models.DecimalField(max_digits=9, decimal_places=6, blank=True)
+    longitude = models.DecimalField(max_digits=9, decimal_places=6, blank=True)
     creation_date = models.DateTimeField(auto_now=True)
 
     def __unicode__(self):
@@ -96,6 +97,8 @@ class Image(models.Model):
     image_file = models.FileField(upload_to='images/%Y%m%d/')
     image_type = models.CharField(max_length=1, choices=IMAGE_TYPES, default=RAW)
     image_format = models.CharField(max_length=3, choices=IMAGE_FORMATS, default=FITS)
+    date = models.DateTimeField(blank=True)
+    exposure = models.DecimalField(max_digits=8, decimal_places=2, blank=True)
     session = models.ForeignKey(Session)
     creation_date = models.DateTimeField(auto_now=True)
 
@@ -104,13 +107,13 @@ class Image(models.Model):
 
 
 class ImageMetadata(models.Model):
-    BOOLEAN = 'BOOLEAN'
-    INTEGER = 'INTEGER'
-    TEXT = 'TEXT'
-    DATE = 'DATE'
-    DECIMAL = 'DECIMAL'
-    FLOAT = 'FLOAT'
-    CHAR = 'CHAR'
+    BOOLEAN = 'B'
+    INTEGER = 'I'
+    TEXT = 'T'
+    DATE = 'D'
+    DECIMAL = '1'
+    FLOAT = 'F'
+    CHAR = 'C'
     VALUE_TYPES = (
         (BOOLEAN, 'Boolean'),
         (INTEGER, 'Integer'),
@@ -122,14 +125,14 @@ class ImageMetadata(models.Model):
     )
     image = models.ForeignKey(Image)
     key = models.CharField(max_length=200, db_index=True)
-    value_type = models.CharField(max_length=200, choices=VALUE_TYPES, default=TEXT)
-    boolean_value = models.NullBooleanField(null=True)
-    integer_value = models.IntegerField(null=True)
-    text_value = models.TextField(null=True)
-    date_value = models.DateTimeField(null=True)
-    decimal_value = models.DecimalField(max_digits=10, decimal_places=2, null=True)
-    float_value = models.FloatField(null=True)
-    char_value = models.CharField(max_length=200, null=True)
+    value_type = models.CharField(max_length=1, choices=VALUE_TYPES, default=TEXT)
+    boolean_value = models.NullBooleanField(blank=True)
+    integer_value = models.IntegerField(blank=True)
+    text_value = models.TextField(blank=True)
+    date_value = models.DateTimeField(blank=True)
+    decimal_value = models.DecimalField(max_digits=10, decimal_places=2, blank=True)
+    float_value = models.FloatField(blank=True)
+    char_value = models.CharField(max_length=200, blank=True)
     creation_date = models.DateTimeField(auto_now=True)
     
     def __unicode__(self):
@@ -139,7 +142,13 @@ class ImageMetadata(models.Model):
 def save_image_metadata(sender, **kwargs):
     """ Stores image metadata in table """
     image = kwargs.get('instance', False)
-    if (image):
+    new = kwargs.get('created', False)
+    if (image and new):
+        ## TODO: update_fields support in Django 1.5
+        ## if ('image_file' in updated_fields):
+        ##    # Delete old entries in ImageMetadata
+        ##    ImageMetadata.objects.filter(image=image).delete()
+        # Read FITS headers and create rows in ImageMetadata
         image_path = join(settings.MEDIA_ROOT, image.image_file.name)
         image_data, header = getdata(image_path, header=True)
         for key in header.keys():
@@ -154,10 +163,22 @@ def save_image_metadata(sender, **kwargs):
                 metadata.boolean_value = value
                 metadata.value_type = metadata.BOOLEAN
             elif (type(value) == str):
-                metadata.text_value = value
-                metadata.value_type = metadata.TEXT
+                if (value.startswith('DATE')):
+                    # It's a date, convert from string
+                    metadata.date_value = parser.pase(value)
+                    metadata.value_type = metadata.DATE
+                else:
+                    metadata.text_value = value
+                    metadata.value_type = metadata.TEXT
             elif (type(value) == float):
                 metadata.float_value = value
-                metadata.value_type = metadata.INTEGER
+                metadata.value_type = metadata.FLOAT
             metadata.save()
+        # Save date and exposure time in Image fields
+        exptime = header.get('EXPTIME', False)
+        date = header.get('DATE', False)
+        if (exptime and date):
+            image.exposure = exptime
+            image.date = parser.parse(date)
+            image.save()
 
